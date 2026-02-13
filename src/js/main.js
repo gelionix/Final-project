11c41207e97f97d50f1124343b8241b4b9fd726f
Oleg Y <shadow7337@hotmail.com>
2026-02-14 00:29:56 +0200

    js added

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,21 @@
 import '../scss/style.scss';
+
+(function () {
+  const tabs = document.querySelectorAll('.tabs__links-item');
+  const content = document.querySelectorAll('.tabs__content-item');
+
+  tabs.forEach((tab, index) => {
+    tab.addEventListener('click', () => {
+      tabs.forEach((tab) => {
+        tab.classList.remove('active');
+      });
+
+      content.forEach((item) => {
+        item.classList.remove('active');
+      });
+
+      tabs[index].classList.add('active');
+      content[index].classList.add('active');
+    });
+  });
+})();
